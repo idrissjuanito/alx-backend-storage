754463416ba390f73aa7961a2d88f1616644fa93
idrissjuanito <idrissjuanito@gmail.com>
2024-03-21 20:13:50 +0100

    task 00

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,8 @@
 -- task is to write an sql query to create a table that has uniq attributes
 
 -- creates a users table
-CREATE TABLE `users` IF NOT EXISTS (
-    `id` int AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `email` varchar(255) NOT NULL UNIQUE,
     `name` varchar(255),
-    PRIMARY KEY(`id`)
 )
